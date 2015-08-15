8de7573baada10090407b86bfa1e7d5a9f002763
Abhi Khatri <abhikhatri391@gmail.com>
2015-08-15 13:08:56 +0530

    Formated data and build resume header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = "Abhi Khatri"
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Frontend Developer"
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
