65740c1ef1eab29e2a175423aa976f5ad729c822
Abhi Khatri <abhikhatri391@gmail.com>
2015-08-15 20:30:21 +0530

    Clean out resumeBuilder for next chapter

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,10 +3,57 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 var name = "Abhi Khatri"
-var formattedName = HTMLheaderName.replace("%data%", name);
-
 var role = "Frontend Developer"
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var skills = ["Frontend Developer", "Thinker", "Entrepreneur", "Art lover"]
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+// var formattedName = HTMLheaderName.replace("%data%", name);
+
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+// 
+
+
+var bio = {
+
+	"name": name,
+	"role": role,
+	"contact": "abhikhatri391@gmail.com",
+	"picture": "http://www.abhikhatri.com/build/img/avtaar.jpg",
+	"skills": skills,
+	"message": "Abhi Khatri is a web developer"
+
+}
+
+
+var education = {
+	"schools": [
+		{
+			"name": "K.G.S.B.V",
+			"degree": "High School Gradutation",
+			"year": "2013",
+			"Subject": "Commerce"
+		},
+
+		{
+			"name": "NIIT",
+			"degree": "Diploma In Computer Science",
+			"year": "2015",
+			"subject": "Computer Science",
+		}
+
+	],
+
+	"onlineCourses": [
+		"name": "Udacity JavaScript Syntax",
+		"language": "JavaScript",
+		"website": "www.udacity.com"
+	]
+
+}
+
+
+var work = {}
+
+var project = {}
